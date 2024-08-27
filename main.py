b8160bc5a5652d35c6eefa22d69e19d07e941351
pizzadeluxe <pizzadeluxe10@gmail.com>
2024-08-27 12:26:44 -0600

    new clock set to 60 fps

--- main.py
+++ main.py
@@ -8,15 +8,20 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
-        screen.fill(color=(0,50,0))
+        screen.fill("black")
         pygame.display.flip()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        dt = clock.tick(60)/1000
+
 
 
 
