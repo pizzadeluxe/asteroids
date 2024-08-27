f4cf3297dde68f4be4e75fea87001fdcf32fa038
pizzadeluxe <pizzadeluxe10@gmail.com>
2024-08-27 12:18:39 -0600

    added game loop and window

--- main.py
+++ main.py
@@ -2,11 +2,23 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init()
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        screen.fill(color=(0,50,0))
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+
 
 if __name__ == "__main__":
     main()
